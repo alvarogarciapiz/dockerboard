897631a6133bfd9674e738cbaf480ad1222f2878
alvarogarciapiz <alvarogp99@gmail.com>
2024-05-03 14:43:12 +0200

    chore: Update dependencies, add file loader for images, and improve dashboard styling

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from '../../public/Dockerboard_logo.png';
+import homeIcon from '../../public/icons/home.png';
+import imageIcon from '../../public/icons/image.png';
+import containerIcon from '../../public/icons/container.png';
+import volumeIcon from '../../public/icons/volume.png';
+import learnIcon from '../../public/icons/learn.png';
 import '../styles/Dashboard.css';
 import Images from './Images.jsx';
 import Containers from './Containers.jsx';
 import Volumes from './Volumes.jsx';
+import Learn from './Learn.jsx';
 
 const Dashboard = () => {
     return (
         <Router>
             <div className="dashboardContainer">
                 <div className="dashboardSidebar">
-                <img src={logo} alt='Logo' className='logo' />
+                <img src={logo} alt='Dockerboard Logo' className='logo' />
                   <nav>
-                    <Link to="/images">Images</Link>
-                    <Link to="/containers">Containers</Link>
-                    <Link to="/volumes">Volumes</Link>
+                    <div className="sidebarLink"><Link to="/"><img src={homeIcon} alt="Home Icon" /> Home</Link></div>
+                    <div className="sidebarLink"><Link to="/images"><img src={imageIcon} alt="Images Icon" /> Images</Link></div>
+                    <div className="sidebarLink"><Link to="/containers"><img src={containerIcon} alt="Containers Icon" /> Containers</Link></div>
+                    <div className="sidebarLink"><Link to="/volumes"><img src={volumeIcon} alt="Volumes Icon" /> Volumes</Link></div>
+                    <div className="sidebarLink"><Link to="/learn"><img src={learnIcon} alt="Learn Icon" /> Learn</Link></div>
                   </nav>
                 </div>
                 <div className="dashboardContent">
@@ -23,6 +31,7 @@ const Dashboard = () => {
                         <Route path="/images" element={<Images />} />
                         <Route path="/containers" element={<Containers />} />
                         <Route path="/volumes" element={<Volumes />} />
+                        <Route path="/learn" element={<Learn />} />
                     </Routes>
                 </div>
             </div>
---
+++ src/components/Learn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Learn.css';
+
+const Learn = () => {
+    return (
+        <>
+        <p>Learn</p>
+        </>
+    );
+};
+
+export default Learn;
